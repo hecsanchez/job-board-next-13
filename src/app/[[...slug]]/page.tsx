25b7481d294d33c3cde629245a0de80528f2eb23
Hector Sanchez <hola@hectorsanchez.mx>
2023-05-04 14:55:26 -0600

    ✨ Setup Listings page

---
+++ src/app/[[...slug]]/page.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/Header";
+
+const ListingsPage = () => {
+
+  return (
+      <>
+        <Header/>
+        <main className="flex min-h-screen flex-col items-center px-3 py-16">
+          <div className="max-w-2xl text-center">
+            <h2 className="text-3xl sm:text-5xl font-bold leading-4xl mb-5">Find the job you are looking for</h2>
+          </div>
+          <div className="max-w-5xl w-full">
+
+          </div>
+        </main>
+      </>
+  )
+}
+
+export default ListingsPage;
